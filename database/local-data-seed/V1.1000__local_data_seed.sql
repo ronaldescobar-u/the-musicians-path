bb0308788b5d0b8c347f93755273c03a29039434
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-26 16:02:41 -0400

    Added missing ,

--- database/local-data-seed/V1.1000__local_data_seed.sql
+++ database/local-data-seed/V1.1000__local_data_seed.sql
@@ -4,7 +4,7 @@ insert into genre(name) values ('Rock'), ('Pop'), ('Hip Hop'), ('Jazz'), ('R&B')
 
 insert into artist(name) values ('The Beatles'), ('Taylor Swift'), ('Kendrick Lamar'), ('Miles Davis'), ('Frank Ocean');
 
-insert into user(first_name, last_name, email, password) values ('John', 'Doe', 'johndoe@email.com', 'password');
+insert into "user"(first_name, last_name, email, password) values ('John', 'Doe', 'johndoe@email.com', 'password');
 
 insert into song(name, artist_id, genre_id, difficulty, added_by)
 values ('I Want To Hold Your Hand', 1, 1, 3, 1),
@@ -13,7 +13,7 @@ values ('I Want To Hold Your Hand', 1, 1, 3, 1),
 ('I Knew You Were Trouble', 2, 2, 4, 1),
 ('Pink + White', 5, 5, 5, 1);
 
-insert into song_files(song_id, file_type_id, value)
+insert into song_file(song_id, file_type_id, value)
 values (1, 1, 'C D G B'),
 (2, 1, 'Am G D'),
 (3, 1, 'C#m G E B'),
@@ -21,7 +21,7 @@ values (1, 1, 'C D G B'),
 (5, 1, 'Fm Bm A C');
 
 insert into course(name, description, added_by)
-values ('The Beatles: Begginers Course', "Only easy songs to play by The Beatles", 1);
+values ('The Beatles: Begginers Course', 'Only easy songs to play by The Beatles', 1);
 
-insert into course_song(course_id, song_id, order, added_by)
+insert into course_song(course_id, song_id, "order", added_by)
 values (1, 1, 1, 1), (1, 2, 2, 1), (1, 3, 3, 1);
