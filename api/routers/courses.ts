17c038ac59b212d78338855306372609d830f6f7
Ronald Escobar <ronald.escobar@unosquare.com>
2023-05-30 17:54:59 -0400

    Added swagger docs for get endpoints in courses

--- api/routers/courses.ts
+++ api/routers/courses.ts
@@ -1,11 +1,92 @@
 import { Request, Router } from 'express';
 
 const coursesRouter = Router();
-
+/**
+* @openapi
+* /courses:
+*   get:
+*     tags: [
+*        courses
+*     ]
+*     parameters:
+*       - name: searchQuery
+*         in: query
+*         type: string
+*         description: Text search query for courses
+*     responses:
+*       200:
+*         description: OK
+*         content:
+*             application/json:
+*                 examples:
+*                     jsonObject:
+*                         summary: List of courses
+*                         value: '[
+*                          { "id": 1, "name": "The Beatles: Begginers Path", "description": "Only easy songs to play by The Beatles", "addedBy": 1 },
+*                          { "id": 2, "name": "80s Classics From Beginner to Advanced", "description": "All 80s songs starting from easy ones going through tough ones at the end", "addedBy": 1 }]'
+*       401:
+*         description: Unauthorized
+*         content:
+*             application/json:
+*                 examples:
+*                     jsonObject:
+*                         summary: An example JSON response
+*                         value: '{ "message": "Unauthorized" }'
+*
+*/
 coursesRouter.route('/').get((request: Request<{}, {}, {}, { searchQuery: string }>, response) => {
   response.send(`Get courses. Query param: ${request.query.searchQuery}`);
 });
 
+/**
+* @openapi
+* /courses/{id}:
+*   get:
+*     tags: [
+*        courses
+*     ]
+*     responses:
+*       200:
+*         description: OK
+*         content:
+*             application/json:
+*                 examples:
+*                     jsonObject:
+*                         summary: List of courses
+*                         value: '{
+*                          "id": 1,
+*                          "name": "The Beatles: Begginers Path",
+*                          "description": "Only easy songs to play by The Beatles",
+*                          "addedBy": 1,
+*                          "averageRating": 4.5,
+*                          "songs": [
+*                            {
+*                              "id": 1,
+*                              "name": "I want to hold your hand",
+*                              "artist": "The Beatles",
+*                              "genre": "Rock",
+*                              "difficulty": 3,
+*                              "addedBy": "John Doe"
+*                            },
+*                            {
+*                              "id": 2,
+*                              "name": "Penny Lane",
+*                              "artist": "The Beatles",
+*                              "genre": "Rock",
+*                              "difficulty": 5,
+*                              "addedBy": "John Doe"
+*                            }
+*                          ]}'
+*       401:
+*         description: Unauthorized
+*         content:
+*             application/json:
+*                 examples:
+*                     jsonObject:
+*                         summary: An example JSON response
+*                         value: '{ "message": "Unauthorized" }'
+*
+ */
 coursesRouter.route('/:id').get((request: Request<{ id: number }>, response) => {
   response.send(`Get courses. Path param: ${request.params.id}`);
 });
@@ -26,6 +107,51 @@ coursesRouter.route('/:id/ratings').delete((request, response) => {
   response.send(`Get ratings of course: ${request.params.id}`);
 });
 
+/**
+* @openapi
+* /courses/{id}/ratings:
+*   get:
+*     tags: [
+*        courses
+*     ]
+*     responses:
+*       200:
+*         description: OK
+*         content:
+*             application/json:
+*                 examples:
+*                     jsonObject:
+*                         summary: List of courses
+*                         value: '[{
+*                            "id": 1,
+*                            "user": {
+*                              "firstName": "John",
+*                              "lastName": "Doe",
+*                              "email": "johndoe@email.com"
+*                            },
+*                            "stars": 5,
+*                            "text": "Amazing course!"
+*                          },
+*                          {
+*                            "id": 2,
+*                            "user": {
+*                              "firstName": "Jane",
+*                              "lastName": "Doe",
+*                              "email": "janedoe@email.com"
+*                            },
+*                            "stars": 3,
+*                            "text": null
+*                          }]'
+*       401:
+*         description: Unauthorized
+*         content:
+*             application/json:
+*                 examples:
+*                     jsonObject:
+*                         summary: An example JSON response
+*                         value: '{ "message": "Unauthorized" }'
+*
+*/
 coursesRouter.route('/:id/ratings').get((request, response) => {
   response.send(`Get ratings of course: ${request.params.id}`);
 });
