33341bc608a2405c3f97a26700501b7ef079d96e
Ronald Escobar <ronald.escobar@unosquare.com>
2023-06-14 09:26:45 -0400

    Added test for artists and genres.

---
+++ api/__tests__/artists.test.ts
@@ -0,0 +1,26 @@
+import { describe, jest, it, expect } from '@jest/globals';
+import { artistsController } from '../controllers';
+import prismaClient from '../prisma/client';
+
+jest.mock('@prisma/client');
+jest.mock('../prisma/client')
+
+describe('artists controller', () => {
+  describe('getArtists', () => {
+    const artists = [{ id: 1, name: 'The Beatles' }];
+
+    it('should return artists and call findMany', async () => {
+      const req = {};
+      const res = {
+        json: jest.fn()
+      };
+      const prismaClientAsAny = prismaClient as any;
+      prismaClientAsAny.artist = { findMany: jest.fn().mockReturnValueOnce(artists) };
+
+      await artistsController.getArtists(req as any, res as any);
+
+      expect(prismaClientAsAny.artist.findMany).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith(artists);
+    });
+  });
+});
---
+++ api/__tests__/genres.test.ts
@@ -0,0 +1,26 @@
+import { describe, jest, it, expect } from '@jest/globals';
+import { genresController } from '../controllers';
+import prismaClient from '../prisma/client';
+
+jest.mock('@prisma/client');
+jest.mock('../prisma/client')
+
+describe('genres controller', () => {
+  describe('getGenres', () => {
+    const genres = [{ id: 1, name: 'Rock' }];
+
+    it('should return genres and call findMany', async () => {
+      const req = {};
+      const res = {
+        json: jest.fn()
+      };
+      const prismaClientAsAny = prismaClient as any;
+      prismaClientAsAny.genre = { findMany: jest.fn().mockReturnValueOnce(genres) };
+
+      await genresController.getGenres(req as any, res as any);
+
+      expect(prismaClientAsAny.genre.findMany).toHaveBeenCalled();
+      expect(res.json).toHaveBeenCalledWith(genres);
+    });
+  });
+});
