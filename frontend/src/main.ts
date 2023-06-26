20d025fd6978be9b5bd4115be35b400fc149add0
Ronald Escobar <ronald.escobar@unosquare.com>
2023-06-26 09:44:14 -0400

    Setup vue router with routes for login and register. Added components login and register. Added some routerlinks and routerview to app.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,23 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import Login from './pages/Login.vue';
+import Register from './pages/Register.vue';
+import VueRouter from 'vue-router'
+
+const routes = [
+  { path: '/login', component: Login },
+  { path: '/register', component: Register }
+];
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes
+});
 
 const vuetify = createVuetify({
   components,
   directives,
 });
 
-createApp(App).use(vuetify).mount('#app');
+createApp(App).use(vuetify).use(router).mount('#app');
