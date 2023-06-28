98dfa989a810b80f9a9e104608febcb63b538966
Ronald Escobar <ronald.escobar@unosquare.com>
2023-06-28 12:43:47 -0400

    Added layout for signup based on login. Added state for signup and empty function.

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -2,8 +2,6 @@
 import { ref } from 'vue';
 import axios from 'axios';
 
-defineProps<{ msg: string }>()
-
 const email = ref('');
 const password = ref('');
 const passwordVisible = ref(false);
@@ -19,15 +17,13 @@ function login() {
         Login
       </v-card-title>
       <v-text-field v-model="email" label="Email"></v-text-field>
-      <v-text-field v-model="password"
-      :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="passwordVisible ? 'text' : 'password'"
-        @click:append-inner="passwordVisible = !passwordVisible"
+      <v-text-field v-model="password" :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="passwordVisible ? 'text' : 'password'" @click:append-inner="passwordVisible = !passwordVisible"
         label="Password">
       </v-text-field>
-        <v-btn block @click="login" variant="elevated" color="indigo-accent-2">
-          Login
-        </v-btn>
+      <v-btn block @click="login" variant="elevated" color="indigo-accent-2">
+        Login
+      </v-btn>
       <p class="text-center mt-4 mb-2">Dont have an account yet? <router-link to='/register'>Sign up</router-link></p>
     </v-card>
   </div>
--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -1,12 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const firstName = ref('');
+const lastName = ref('');
+const email = ref('');
+const password = ref('');
+const passwordVisible = ref(false);
+const confirmPassword = ref('');
+const confirmPasswordVisible = ref(false);
+
+function signUp() {
+
+}
+</script>
 <template>
-  <h3>Register</h3>
-  <v-text-field label="First name"></v-text-field>
-  <v-text-field label="Last name"></v-text-field>
-  <v-text-field label="Email"></v-text-field>
-  <v-text-field label="Password"></v-text-field>
-  <v-text-field label="Confirm password"></v-text-field>
-  <v-btn>
-    Sign up
-  </v-btn>
-  <p>Already have an account? <router-link to='/login'>Login</router-link></p>
+  <div class="bg-indigo-accent-2 h-screen py-10">
+    <v-card class="mx-auto px-6 py-4" max-width="450">
+      <v-card-title class="font-weight-bold">
+        Sign up
+      </v-card-title>
+      <v-text-field v-model="firstName" label="First name"></v-text-field>
+      <v-text-field v-model="lastName" label="Last name"></v-text-field>
+      <v-text-field v-model="email" label="Email"></v-text-field>
+      <v-text-field
+        v-model="password"
+        :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="passwordVisible ? 'text' : 'password'"
+        @click:append-inner="passwordVisible = !passwordVisible"
+        label="Password"
+      >
+      </v-text-field>
+      <v-text-field
+        v-model="confirmPassword"
+        :append-inner-icon="confirmPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="confirmPasswordVisible ? 'text' : 'password'"
+        @click:append-inner="confirmPasswordVisible = !confirmPasswordVisible"
+        label="Confirm password"
+      >
+      </v-text-field>
+      <v-btn block @click="signUp" variant="elevated" color="indigo-accent-2">
+        Sign up
+      </v-btn>
+      <p class="text-center mt-4 mb-2">Already have an account? <router-link to='/login'>Login</router-link></p>
+    </v-card>
+  </div>
 </template>
