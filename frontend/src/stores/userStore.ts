03e299e8989494aa87ef97675e51be65497548e2
Ronald Escobar <ronald.escobar@unosquare.com>
2023-07-10 10:53:20 -0400

    Added logic to open modal. Installed jwt decode. Added user store

---
+++ frontend/src/stores/userStore.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+import User from "../types/User";
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: null as User | null
+  }),
+  actions: {
+    setUser(user: User | null) {
+      this.user = user;
+    }
+  }
+});
